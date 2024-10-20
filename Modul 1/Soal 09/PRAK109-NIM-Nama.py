76dcfbd925dff888942cd88df01fea3620535cc0
LittlePien <abdulmalikfajar14@gmail.com>
2024-10-20 22:11:39 +0800

    PRAK109-NIM-Nama.py

--- Modul 1/Soal 09/PRAK109-NIM-Nama.py
+++ Modul 1/Soal 09/PRAK109-NIM-Nama.py
@@ -1,7 +1,6 @@
-pasukan = 958730
-pahlawan = 5
+pasukan = int(input("Jumlah pasukan yang dibawa Yu Zhong = "))
+pahlawan = int(input("Jumlah pahlawan = "))
+
 hasil = pasukan / pahlawan
 
-print("Jumlah pasukan yang dibawa Yu Zhong =", pasukan)
-print("Jumlah pahlawan =", pahlawan)
 print(f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {hasil:.0f} pasukan")
